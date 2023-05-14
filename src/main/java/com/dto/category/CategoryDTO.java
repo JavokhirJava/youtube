47362c7a1a2bf851afe189a13fadec3f9adb7f80
Muhammad <soliyevmuhammad137@gmail.com>
2023-05-14 07:46:17 -0700

    Categorydto commit

---
+++ src/main/java/com/dto/category/CategoryDTO.java
@@ -0,0 +1,17 @@
+package com.dto.category;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class CategoryDTO {
+        private Integer id;
+        private String name;
+        private String nameUZ;
+        private String nameRU;
+        private String nameEN;
+        private Boolean visible;
+        private LocalDateTime createdDate;
+}
