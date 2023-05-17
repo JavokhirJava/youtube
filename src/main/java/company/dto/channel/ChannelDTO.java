4c1dd51c5afd9f869c9a4925de947bdfe8f5631c
JavokhirJava <abdinabiyev2001@gmail.com>
2023-05-17 16:47:11 +0500

    channel request and response (dto)

--- src/main/java/company/dto/channel/ChannelDTO.java
+++ src/main/java/company/dto/channel/ChannelDTO.java
@@ -1,11 +1,13 @@
 package company.dto.channel;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChannelDTO {
     private String id;
     @NotNull(message = "name required")
@@ -13,4 +15,5 @@ public class ChannelDTO {
     private String description;
     private String photoId;
     private String bannerId;
+    private Integer profileId;
 }
